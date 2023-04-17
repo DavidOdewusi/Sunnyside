d5cdde0557004a5f3acf56228629b6735b6932eb
David Odewusi <davidodewusi@gmail.com>
2023-04-17 22:50:47 +0100

    js

--- script.js
+++ script.js
@@ -14,15 +14,19 @@ const arrowDown = document.querySelector(".hero__img");
 //   });
 // });
 
-document.querySelector(".nav__list").addEventListener("click", function (e) {
-  e.preventDefault();
+const scroll = function (el) {
+  document.querySelector(el).addEventListener("click", function (e) {
+    e.preventDefault();
 
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    console.log(id);
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  }
-});
+    if (e.target.classList.contains("nav__link")) {
+      const id = e.target.getAttribute("href");
+      console.log(id);
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  });
+};
+scroll(".nav__list");
+scroll(".menu");
 
 arrowDown.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
