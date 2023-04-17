2bf481e6af3b0e9d519811870f36a0f28458ab8d
David Odewusi <davidodewusi@gmail.com>
2023-04-17 15:28:05 +0100

    js

--- script.js
+++ script.js
@@ -1,50 +1,19 @@
 "use scrict";
 
-const about = document.querySelectorAll(".about-link");
-const service = document.querySelectorAll(".service-link");
-const project = document.querySelectorAll(".project-link");
+const links = document.querySelectorAll(".nav__link");
 const section1 = document.querySelector("#section--1");
-const section2 = document.querySelector("#section--2");
-const section3 = document.querySelector("#section--3");
 const arrowDown = document.querySelector(".hero__img");
 
-about.forEach((e) => {
+links.forEach((e) => {
   e.addEventListener("click", function (e) {
-    const s1cord = section1.getBoundingClientRect();
-    console.log(s1cord);
-    window.scrollTo({
-      left: s1cord.left + pageXOffset,
-      top: s1cord.top + pageYOffset,
-      behavior: "smooth",
-    });
-    //   section2.scrollIntoView({ behavior: "smooth" });
+    console.log(e);
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   });
 });
 
-service.forEach((e) => {
-  e.addEventListener("click", function (e) {
-    const s2cord = section2.getBoundingClientRect();
-    console.log(s2cord);
-    window.scrollTo({
-      left: s2cord.left + pageXOffset,
-      top: s2cord.top + pageYOffset,
-      behavior: "smooth",
-    });
-    //   section2.scrollIntoView({ behavior: "smooth" });
-  });
-});
-project.forEach((e) => {
-  e.addEventListener("click", function (e) {
-    const s3cord = section3.getBoundingClientRect();
-    console.log(s3cord);
-    window.scrollTo({
-      left: s3cord.left + pageXOffset,
-      top: s3cord.top + pageYOffset,
-      behavior: "smooth",
-    });
-    //   section2.scrollIntoView({ behavior: "smooth" });
-  });
-});
 arrowDown.addEventListener("click", function (e) {
   const s1cord = section1.getBoundingClientRect();
   console.log(s1cord);
