2695b6e2ea6c8b1eb340681bdb7a9a9b3f262aaa
David Odewusi <davidodewusi@gmail.com>
2023-04-17 16:00:00 +0100

    'js'

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const links = document.querySelectorAll(".nav__link");
 const section1 = document.querySelector("#section--1");
 const arrowDown = document.querySelector(".hero__img");
 
-links.forEach((e) => {
-  e.addEventListener("click", function (e) {
-    console.log(e);
-    e.preventDefault();
+// links.forEach((e) => {
+//   e.addEventListener("click", function (e) {
+//     console.log(e);
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+document.querySelector(".nav__list").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav__link")) {
     const id = this.getAttribute("href");
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
 
 arrowDown.addEventListener("click", function (e) {
